f1a5209e5e154e4ada11b4388702ee51d07ee061
Rajamanickam <89737788+erajamanickam@users.noreply.github.com>
2022-05-15 14:08:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+    let internet = document.getElementById("internet");
+    if(navigator.onLine){
+        internet.textContent = "We are Online!";
+        internet.innerHTML = "<i class='uil uil-wifi'></i> You're online now"
+        internet.style.color = "green";
+    }
+    window.addEventListener("online",function(){
+        internet.textContent = "we are online";
+        internet.style.color = "green";
+    })
+    window.addEventListener("offline",function(){
+        internet.textContent = "<i class='uil uil-wifi-slash'></i> You're Offline now"
+        internet.style.color = "red";
+    });
